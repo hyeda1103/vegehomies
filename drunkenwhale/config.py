fbdccd161d7a8d3ce327e2756361d4c98fe8e859
hyeda1103 <dalgona92@gmail.com>
2020-10-11 01:06:09 +0900

    Create config.py

---
+++ drunkenwhale/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SECRET_KEY = 'secretkey'
+    SQLALCHEMY_DATABASE_URI = 'database_uri'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER='mailserver'
+    MAIL_PORT= 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME='username'
+    MAIL_PASSWORD='password'
